c2bbc63185fe140be254e9a5b9bc85f65be66b99
Michael Taufen <mtaufen@gmail.com>
2016-04-25 10:28:01 -0700

    cleanup

--- user/vmm/virtio_mmio.c
+++ user/vmm/virtio_mmio.c
@@ -103,111 +103,68 @@ uint32_t virtio_mmio_rd_reg(struct virtio_mmio_dev *mmio_dev, uint64_t gpa)
 
 // TODO: the spec I am referencing is: http://docs.oasis-open.org/virtio/virtio/v1.0/cs04/virtio-v1.0-cs04.html
 	switch(offset) {
-/*
-Magic value
-0x74726976 (a Little Endian equivalent of the “virt” string).
-*/
+		// Magic value
+		// 0x74726976 (a Little Endian equivalent of the “virt” string).
 		case VIRTIO_MMIO_MAGIC_VALUE:
 			return VIRT_MAGIC;
-/*
-Device version number
-0x2. Note: Legacy devices (see 4.2.4 Legacy interface) used 0x1.
-*/
+
+		// Device version number
+		// 0x2. Note: Legacy devices (see 4.2.4 Legacy interface) used 0x1.
 		case VIRTIO_MMIO_VERSION:
 			return VIRT_MMIO_VERSION;
-/*
-Virtio Subsystem Device ID
-See 5 Device Types for possible values. Value zero (0x0) is used to define a
-system memory map with placeholder devices at static, well known addresses,
-assigning functions to them depending on user’s needs.
-*/
+
+		// Virtio Subsystem Device ID (see virtio-v1.0-cs04 sec. 5 for values)
+		// Value 0x0 is used to define a system memory map with placeholder
+		// devices at static, well known addresses.
 		case VIRTIO_MMIO_DEVICE_ID:
 			return mmio_dev->vqdev->dev_id;
-/*
-Virtio Subsystem Vendor ID
-*/
+
+		// Virtio Subsystem Vendor ID
 		case VIRTIO_MMIO_VENDOR_ID:
 			return VIRT_MMIO_VENDOR;
-/*
-Flags representing features the device supports
-Reading from this register returns 32 consecutive flag bits, the least significant
-bit depending on the last value written to DeviceFeaturesSel. Access to this register
-returns bits DeviceFeaturesSel ∗ 32 to (DeviceFeaturesSel ∗ 32) + 31, eg. feature
-bits 0 to 31 if DeviceFeaturesSel is set to 0 and features bits 32 to 63 if
-DeviceFeaturesSel is set to 1. Also see 2.2 Feature Bits.
-*/
+
+		// Flags representing features the device supports
 		case VIRTIO_MMIO_DEVICE_FEATURES:
 			if (!(mmio_dev->status & VIRTIO_CONFIG_S_DRIVER))
 				VIRTIO_DRI_ERRX(mmio_dev->vqdev,
 				         "Attempt to read device features before setting the DRIVER status bit. See virtio-v1.0-cs04 sec. 3.1.1.");
-
 			if (mmio_dev->dev_feat_sel) // high 32 bits requested
 				return mmio_dev->vqdev->dev_feat >> 32;
-
 			return mmio_dev->vqdev->dev_feat; // low 32 bits requested
-/*
-Maximum virtual queue size
-Reading from the register returns the maximum size (number of elements) of the queue
-the device is ready to process or zero (0x0) if the queue is not available. This applies
-to the queue selected by writing to QueueSel.
-*/
+
+		// Maximum virtual queue size
+		// Returns the maximum size (number of elements) of the queue the device
+		// is ready to process or zero (0x0) if the queue is not available.
+		// Applies to the queue selected by writing to QueueSel.
 		case VIRTIO_MMIO_QUEUE_NUM_MAX:
-			// TODO: Spec says to return 0 if the queue is not available
-			// TODO: Exactly what do they mean by "available"?
-			// TODO: For now, we are assuming that if you gave a vqdev
-			//       to the mmio_dev, the queues on it are "available."
-			//       I am going to guard against the qsel being
-			//       greater than the num_vqs on the vq_def, however.
-			//       Since queues above this number don't exist, they
-			//       definitely are not available.
-			// Queue indices start at 0
-		// TODO: Is not checking mmio_dev->vqdev->vqs[mmio_dev->qsel].qready
-		//       the right thing to do here?
-			if (mmio_dev->qsel >= mmio_dev->vqdev->num_vqs)
+		// TODO: Is !qready the same as not available?
+			if (mmio_dev->qsel >= mmio_dev->vqdev->num_vqs
+				|| !mmio_dev->vqdev->vqs[mmio_dev->qsel].qready)
 				return 0;
 			return mmio_dev->vqdev->vqs[mmio_dev->qsel].qnum_max;
-/*
-Virtual queue ready bit
-Writing one (0x1) to this register notifies the device that it can execute requests from
-this virtual queue. Reading from this register returns the last value written to it. Both
-read and write accesses apply to the queue selected by writing to QueueSel.
-*/
+
+		// Virtual queue ready bit
+		// Applies to the queue selected by writing to QueueSel.
 		case VIRTIO_MMIO_QUEUE_READY:
 			if (mmio_dev->qsel >= mmio_dev->vqdev->num_vqs)
 				return 0;
 			return mmio_dev->vqdev->vqs[mmio_dev->qsel].qready;
-/*
-Interrupt status
-Reading from this register returns a bit mask of events that caused the
-device interrupt to be asserted. The following events are possible:
 
-Used Ring Update
-- bit 0: the interrupt was asserted because the device has updated the Used Ring in
-         at least one of the active virtual queues.
-
-Configuration Change
-- bit 1: the interrupt was asserted because the configuration of
-         the device has changed.
-*/
+		// Interrupt status
+		// Bit mask of events that caused the device interrupt to be asserted.
+		// bit 0: Used Ring Update
+		// bit 1: Configuration Change
 		case VIRTIO_MMIO_INTERRUPT_STATUS:
 			return mmio_dev->isr;
-/*
-Device status
-Reading from this register returns the current device status flags. Writing non-zero values
-to this register sets the status flags, indicating the driver progress. Writing zero (0x0)
-to this register triggers a device reset. See also p. 4.2.3.1 Device Initialization.
-*/
+
+		// Device status
 		case VIRTIO_MMIO_STATUS:
 			return mmio_dev->status;
-/*
-Configuration atomicity value
-Reading from this register returns a value describing a version of the device-specific
-configuration space (see Config). The driver can then access the configuration space and,
-when finished, read ConfigGeneration again. If no part of the configuration space has changed
-between these two ConfigGeneration reads, the returned values are identical. If the values are
-different, the configuration space accesses were not atomic and the driver has to perform the
-operations again. See also 2.3.
-*/
+
+		// Configuration atomicity value
+		// Contains a version for the device-specific configuration space
+		// The driver checks this version before and after accessing the config
+		// space, and if the values don't match it repeats the access.
 		case VIRTIO_MMIO_CONFIG_GENERATION:
 			return mmio_dev->cfg_gen;
 
