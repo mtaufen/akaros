fd17b80c88a3237d485b5a0fdb935bfbadce1aa3
Michael Taufen <mtaufen@gmail.com>
2016-04-27 07:59:33 -0700

    cleanup virtio.c comments and errors

--- user/vmm/virtio.c
+++ user/vmm/virtio.c
@@ -45,13 +45,14 @@ static uint32_t get_next_desc(struct vring_desc *desc, uint32_t i, uint32_t max
 	if (next >= max) {
 		VIRTIO_DRI_ERRX(vqdev,
 			"The next descriptor index in the chain provided by the driver is"
-			" greater than the maximum allowed size of its queue.");
+			" outside than the maximum allowed size of its queue.");
 	}
 
 	return next;
 }
 
 // TODO: Rename this fn
+// TODO: Need to make sure we don't overflow iov
 // Based on wait_for_vq_desc in Linux lguest.c
 uint32_t virtio_next_avail_vq_desc(struct virtio_vq *vq, struct iovec iov[],
                             uint32_t *olen, uint32_t *ilen)
@@ -60,44 +61,36 @@ uint32_t virtio_next_avail_vq_desc(struct virtio_vq *vq, struct iovec iov[],
 	struct vring_desc *desc;
 	eventfd_t event;
 
-	// The first thing we do is read from the eventfd. If nothing has been written to it yet,
-	// then the driver isn't done setting things up and we want to wait for it to finish.
-	// For example, dereferencing the vq->vring.avail pointer could segfault if the driver
-	// has not yet written a valid address to it.
+	// The first thing we do is read from the eventfd. If nothing has been
+	// written to it yet, then the driver isn't done setting things up and we
+	// want to wait for it to finish.
+	// For example, dereferencing the vq->vring.avail pointer could segfault if
+	// the driver has not yet written a valid address to it.
 	if (eventfd_read(vq->eventfd, &event))
-		printf("next_avail_vq_desc event read failed?\n");
-	// TODO: I think I want a memory barrier here? In case the event fd gets written but the avail idx hasn't yet?
+		VIRTIO_DEV_ERRX(vq->vqdev,
+			"eventfd read failed while waiting for available descriptors\n");
+	// make sure vring.avail->idx has had a
+	// chance to update before we read it
 	mb();
 
 	while (vq->last_avail == vq->vring.avail->idx) {
-		// If we got poked but there are no queues available, then just return 0
-		// this will work for now because we're only in these handlers during an
-		// exit due to EPT viol. due to driver write to QUEUE_NOTIFY register on dev.
-		// return 0;
 
-
-
-		// TODO: We will move from just returning zero to just sleeping
-		//       again if we were kicked but nothing is available.
 		// NOTE: I do not kick the guest with an irq here. I do that in
 		//       the individual service functions when it is necessary.
 
-		// TODO: What to do here about VRING_DESC_F_NO_NOTIFY flag?
-		// NOTE: If you look at the comments in virtio_ring.h, the VRING_DESC_F_NO_NOTIFY
-		//       flag is set by the host to say to the guest "Don't kick me when you add
-		//       a buffer." But this comment also says that it is an optimization, is not
-		//       always reliable, and that the guest will still kick the host when out of
-		//       buffers. So I'm leaving that out for now, and we can revisit why it might
-		//       improve performance sometime in the future.
-		//       TODO: That said, I might still need to unset the bit. It should be unset
-		//             by default, because it is only supposed to be set by the host and
-		//             I never set it. But this is worth double-checking.
+		// NOTE: If you look at the comments in virtio_ring.h, the
+		//       VRING_DESC_F_NO_NOTIFY flag is set by the host to say to the
+		//       guest "Don't kick me when you add a buffer." But this comment
+		//       also says that it is an optimization, is not always reliable,
+		//       and that the guest will still kick the host when out of
+		//       buffers. So I'm leaving that out for now, and we can revisit
+		//       why it might improve performance sometime in the future.
 
-// TODO: make this a real error
 		if (eventfd_read(vq->eventfd, &event))
-			printf("next_avail_vq_desc event read failed?\n");
-
-		// TODO: Do I want a memory barrier here? In case the event fd gets written but the avail idx hasn't yet?
+			VIRTIO_DEV_ERRX(vq->vqdev,
+				"eventfd read failed while waiting for available descriptors\n");
+		// make sure vring.avail->idx has had a
+		// chance to update before we read it
 		mb();
 	}
 
@@ -112,7 +105,6 @@ uint32_t virtio_next_avail_vq_desc(struct virtio_vq *vq, struct iovec iov[],
 	*/
 
 	// Mod because it's a *ring*
-	// TODO: maybe switch to using vq->vring.num for ours too
 	head = vq->vring.avail->ring[vq->last_avail % vq->vring.num];
 	vq->last_avail++;
 
@@ -123,8 +115,11 @@ uint32_t virtio_next_avail_vq_desc(struct virtio_vq *vq, struct iovec iov[],
 	// Don't know how many output buffers or input buffers there are yet, depends on desc chain.
 	*olen = *ilen = 0;
 
-	max = vq->vring.num; // Since vring.num is the size of the queue, max is the most buffers we could possibly find
-	desc = vq->vring.desc; // qdesc is the address of the descriptor (array?TODO) set by the driver
+	// Since vring.num is the size of the queue, max is the max descriptors
+	// that should be in a descriptor chain. If we find more than that, the
+	// driver is doing something wrong.
+	max = vq->vring.num;
+	desc = vq->vring.desc;
 	i = head;
 
 	// TODO: lguest manually checks that the pointers on the vring fields aren't goofy when the driver
@@ -138,11 +133,11 @@ uint32_t virtio_next_avail_vq_desc(struct virtio_vq *vq, struct iovec iov[],
 
 
 	do {
-
 		// If it's an indirect descriptor, we travel through the layer of indirection and then
 		// we're at table of descriptors chained by `next`, and since they are chained we can
 		// handle them the same way as direct descriptors once we're through that indirection.
 		if (desc[i].flags & VRING_DESC_F_INDIRECT) {
+
 			// virtio-v1.0-cs04 s2.4.5.3.1 Indirect Descriptors
 			if (!(vq->vqdev->dri_feat & (1<<VIRTIO_RING_F_INDIRECT_DESC)))
 				VIRTIO_DRI_ERRX(vq->vqdev,
@@ -165,7 +160,8 @@ uint32_t virtio_next_avail_vq_desc(struct virtio_vq *vq, struct iovec iov[],
 			// virtio-v1.0-cs04 s2.4.5.3.1 Indirect Descriptors
 			if (desc[i].flags & VRING_DESC_F_NEXT)
 				VIRTIO_DRI_ERRX(vq->vqdev,
-					"The driver must not set both the INDIRECT and NEXT flags on a descriptor."
+					"The driver must not set both the INDIRECT and NEXT flags"
+					" on a descriptor."
 					" See virtio-v1.0-cs04 s2.4.5.3.1 Indirect Descriptors");
 
 			// TODO: Better error message, more descriptive of what this error indicates.
@@ -177,7 +173,8 @@ uint32_t virtio_next_avail_vq_desc(struct virtio_vq *vq, struct iovec iov[],
 			//       refers to an indirect table. So we ignore it.
 
 			max = desc[i].len / sizeof(struct vring_desc);
-			desc = (void *)desc[i].addr; // TODO: check that this pointer isn't goofy
+			 // TODO: should check that addresses provided by the guest are neither invalid nor outside guest memory bounds
+			desc = (void *)desc[i].addr;
 			i = 0;
 
 			// virtio-v1.0-cs04 s2.4.5.3.1 Indirect Descriptors
@@ -194,22 +191,21 @@ uint32_t virtio_next_avail_vq_desc(struct virtio_vq *vq, struct iovec iov[],
 		// TODO: You better make sure you pass a big enough scatterlist to this function
 		//       for whatever the eventual value of *olen + *ilen will be!
 		iov[*olen + *ilen].iov_len = desc[i].len;
-		iov[*olen + *ilen].iov_base = (void *)desc[i].addr; // NOTE: .v is basically our scatterlist/iovec's iov_base
+		// TODO: should check that addresses provided by the guest are neither invalid nor outside guest memory bounds
+		iov[*olen + *ilen].iov_base = (void *)desc[i].addr;
 
 		if (desc[i].flags & VRING_DESC_F_WRITE) {
 			// input descriptor, increment *ilen
 			(*ilen)++;
 		}
 		else {
-			// output descriptor, check that this is *before* we read any input descriptors
-			// and then increment *olen if we're ok
-
 			// virtio-v1.0-cs04 s2.4.4.2 Message Framing
 			if (*ilen) {
 				VIRTIO_DRI_ERRX(vq->vqdev,
-					"Device detected an output descriptor after an input descriptor."
-					" The driver must place any device-writeable descriptor elements"
-					" after all device-readable descriptor elements.");
+					"Device detected an output descriptor after an input"
+					" descriptor. The driver must place any device-writeable"
+					" descriptor elements after all device-readable descriptor"
+					" elements.");
 			}
 
 			(*olen)++;
@@ -219,7 +215,9 @@ uint32_t virtio_next_avail_vq_desc(struct virtio_vq *vq, struct iovec iov[],
 		if (*olen + *ilen > max) {
 			VIRTIO_DRI_ERRX(vq->vqdev,
 				"The driver must ensure that there are no loops in the"
-				" descriptor chain it provides!");
+				" descriptor chain it provides! The combined length of"
+				" readable and writeable buffers was greater than the"
+				" number of elements in the queue.");
 		}
 
 
