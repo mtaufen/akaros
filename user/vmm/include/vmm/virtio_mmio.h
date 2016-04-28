f8c6568e0639a7fa9630621c670aa3d46012c1f3
Michael Taufen <mtaufen@gmail.com>
2016-04-28 10:31:20 -0700

    fixup

--- user/vmm/include/vmm/virtio_mmio.h
+++ user/vmm/include/vmm/virtio_mmio.h
@@ -179,7 +179,8 @@ void virtio_mmio_set_vring_irq(struct virtio_mmio_dev *mmio_dev);
 // virtio_mmio_rd_reg and virtio_mmio_wr_reg are used to process the guest's driver's
 // reads and writes to the mmio device registers. gpa is the guest physical address
 // that the driver tried to write to; this is used to calculate the target register
+// size should be the size of the access in bytes
 uint32_t virtio_mmio_rd(struct virtio_mmio_dev *mmio_dev,
-                        uint64_t gpa, uint16_t size);
+                        uint64_t gpa, uint8_t size);
 void     virtio_mmio_wr(struct virtio_mmio_dev *mmio_dev,
-                        uint64_t gpa, uint16_t size, uint32_t *value);
+                        uint64_t gpa, uint8_t size, uint32_t *value);
--- user/vmm/virtio_mmio.c
+++ user/vmm/virtio_mmio.c
@@ -131,7 +131,7 @@ uint32_t virtio_mmio_rd(struct virtio_mmio_dev *mmio_dev,
 	}
 
 	// virtio-v1.0-cs04 4.2.2.2 MMIO Device Register Layout
-	if (size != 32 || !(offset % 32)) {
+	if (size != 4 || !(offset % 4)) {
 		VIRTIO_DRI_ERRX(mmio_dev->vqdev,
 			"w:%u ofst:%u ofst%32:%u \nThe driver must only use 32 bit wide and aligned reads for"
 			" reading the control registers on the MMIO transport.",
@@ -302,7 +302,7 @@ void virtio_mmio_wr(struct virtio_mmio_dev *mmio_dev, uint64_t gpa,
 	}
 
 	// virtio-v1.0-cs04 4.2.2.2 MMIO Device Register Layout
-	if (size != 32 || !(offset % 32)) {
+	if (size != 4 || !(offset % 4)) {
 		VIRTIO_DRI_ERRX(mmio_dev->vqdev,
 			"The driver must only use 32 bit wide and aligned writes for"
 			" writing the control registers on the MMIO transport.");
